11d8acd8ecd66f70c39d8fa5beffe16c8d748f55
vonnenaut <danielashcom@gmail.com>
2020-04-21 15:19:52 +0700

    u/d Gemfile, cms.rb, *.txt file contents added, simplified application.css, added file.erb and updated index.erb and layout.erb

--- cms.rb
+++ cms.rb
@@ -1,13 +1,32 @@
 # cms.rb
 require "sinatra"
 require "sinatra/reloader"
-require "sinatra/content_for"
 require "tilt/erubis"
 
 root = File.expand_path("..", __FILE__)
 
+helpers do
+  def load_file(path)    
+    file = File.open(path, 'r')
+    return file if file
+
+    session[:error] = "The specified file was not found."
+    redirect "/"
+  end
+end
+
 get "/" do
-  
+  @files = Dir.glob(root + "/data/*").map do |path|
+    File.basename(path)
+  end
   
   erb :index
+end
+
+get "/:file" do
+  @file_name = params[:file]
+  path = root + "/data/#{@file_name}"
+  @file_contents = load_file(path)
+
+  erb :file
 end
