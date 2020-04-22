7261bfe49f933dc99c9801efa9b5c71ffb7bc973
vonnenaut <danielashcom@gmail.com>
2020-04-22 18:10:21 +0700

    u/d: renmaed about.txt -> about.md, added markdown, updated tests, edit and index erb, cms.rb, Gemfile

--- cms.rb
+++ cms.rb
@@ -2,8 +2,31 @@
 require "sinatra"
 require "sinatra/reloader"
 require "tilt/erubis"
+require "pathname"
+require "redcarpet"
+
+configure do
+  enable :sessions
+  set :session_secret, 'secret'
+end
 
 root = File.expand_path("..", __FILE__)
+
+def render_markdown(text)
+  markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML)
+  markdown.render(text)
+end
+
+def load_file_content(path)
+  content = File.read(path)
+  case File.extname(path)
+  when ".txt"
+    headers["Content-Type"] = "text/plain"
+    content
+  when ".md"
+    render_markdown(content)
+  end
+end
 
 get "/" do
   @files = Dir.glob(root + "/data/*").map do |path|
@@ -15,6 +38,29 @@ end
 
 get "/:filename" do
   path = root + "/data/" + params[:filename]
-  headers["Content-Type"] = "text/plain"
-  File.read(path)
+
+  if File.exist?(path)
+    load_file_content(path)
+  else 
+    session[:message] = "#{params[:filename]} does not exist."
+    redirect "/"
+  end
+end
+
+get "/:filename/edit" do
+  path = root + "/data/" + params[:filename]
+  
+  @filename = params[:filename]
+  @content = File.read(path)
+
+  erb :edit
+end
+
+post "/:filename" do
+  path = root + "/data/" + params[:filename]
+
+  File.write(path, params[:content])
+
+  session[:message] = "#{params[:filename]} has been updated"
+  redirect "/"
 end
--- test/cms_test.rb
+++ test/cms_test.rb
@@ -17,11 +17,10 @@ class CmsTest < Minitest::Test
 
     assert_equal 200, last_response.status
     assert_equal "text/html;charset=utf-8", last_response["Content-Type"]
-    assert_includes last_response.body, "about.txt"
+    assert_includes last_response.body, "about.md"
     assert_includes last_response.body, "changes.txt"
     assert_includes last_response.body, "history.txt"
   end
-
 
   def test_viewing_history_document
     get "/history.txt"
@@ -31,19 +30,52 @@ class CmsTest < Minitest::Test
     assert_includes last_response.body, "1993 - Yukihiro Matsumoto dreams up Ruby."
   end
 
-  def test_viewing_about_document
-    get "/about.txt"
+  def test_not_found
+    # get a file that doesn't exist
+    get "/doesnotexist.txt"
+   
+    # verify app redirects user
+    assert_equal 302, last_response.status
 
+    # request page user was redirected to
+    get last_response["location"]
+
+    # check page loaded correctly and displays error message
     assert_equal 200, last_response.status
-    assert_equal "text/plain", last_response["Content-Type"]
-    assert_includes last_response.body, "and an example solution that takes the requirements"
+    assert_includes last_response.body, "doesnotexist.txt does not exist."
+
+    # check that error message disappears on reload of page
+    get "/"
+    refute_includes last_response.body, "The specified file was not found."
   end
 
-  def test_viewing_changes_document
-    get "/changes.txt"
+  def test_viewing_markdown_document
+    get "/about.md"
 
     assert_equal 200, last_response.status
-    assert_equal "text/plain", last_response["Content-Type"]
-    assert_includes last_response.body, "Ruby introduces the (0..) range."
+    assert_equal "text/html;charset=utf-8", last_response["Content-Type"]
+    assert_includes last_response.body, "<h1>A File-based Content Management System (CMS)</h1>"
   end
+
+  def test_editing_document
+    get "/changes.txt/edit"
+
+    assert_equal 200, last_response.status
+    assert_includes last_response.body, "<textarea"
+    assert_includes last_response.body, %q(<button type="submit")
+  end
+
+  # def test_updating_document
+  #   post "/changes.txt", content: "new content"
+
+  #   assert_equal 302, last_response.status
+
+  #   get last_response["location"]
+
+  #   assert_includes last_response.body, "changes.txt has been updated"
+
+  #   get "/changes.txt"
+  #   assert_equal 200, last_response.status
+  #   assert_includes last_response.body, "new content"
+  # end  
 end
