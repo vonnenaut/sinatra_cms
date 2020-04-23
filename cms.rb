a441ebf501643d1b428b93a08d9651d6f97dc637
vonnenaut <danielashcom@gmail.com>
2020-04-23 15:30:34 +0700

    added create document functionality, next step: t/s validation failure to catch empty or already-used names

--- cms.rb
+++ cms.rb
@@ -34,15 +34,29 @@ def load_file_content(path)
   end
 end
 
-get "/" do
+def get_filenames
   pattern = File.join(data_path, "*")
-  @files = Dir.glob(pattern).map do |path|
+
+  Dir.glob(pattern).map do |path|
     File.basename(path)
   end
+end
+
+def error_for_document_name(name)
+  if !(1..100).cover? name.size
+    "A name is required."
+  elsif get_filenames.include? name
+    "File name already exists."
+  end
+end
+
+get "/" do
+  @files = get_filenames
   
   erb :index, layout: :layout
 end
 
+# read a file
 get "/:filename" do
   path = File.join(data_path, params[:filename])
 
@@ -54,6 +68,7 @@ get "/:filename" do
   end
 end
 
+# edit an existing file
 get "/:filename/edit" do
   path = File.join(data_path, params[:filename])
   
@@ -70,4 +85,34 @@ post "/:filename" do
 
   session[:message] = "#{params[:filename]} has been updated"
   redirect "/"
+end
+
+get "/document/new" do
+
+  erb :new_document, layout: :layout
+end
+
+# Create a new, empty, named file
+post "/" do
+  # To-Do: Troubleshoot validation check failure: not catching empty or already-used filename
+  document_name = params[:document_name].strip
+
+  error = error_for_document_name(document_name)
+  puts "error: #{error}"
+
+  if error
+    session[:message] = error
+    erb :new_document, layout: :layout
+  else
+    # create new file with given name
+    filename = params[:document_name]
+    File.open("#{data_path}/#{filename}", "w+")
+    session[:message] ="#{params[:document_name]} was created."
+    redirect "/"
+  end
+
+  # refresh the list of files to be rendered at index.erb
+  @files = get_filenames
+
+  erb :index, layout: :layout
 end
