889c0a05a14425d5bcff3a28f928b0d177e3ec27
vonnenaut <danielashcom@gmail.com>
2020-04-23 10:17:39 +0700

    u/d cms.rb, added testing setup and teardown functionality w/in test folder for separate production and testing data files

--- cms.rb
+++ cms.rb
@@ -2,7 +2,6 @@
 require "sinatra"
 require "sinatra/reloader"
 require "tilt/erubis"
-require "pathname"
 require "redcarpet"
 
 configure do
@@ -10,7 +9,13 @@ configure do
   set :session_secret, 'secret'
 end
 
-root = File.expand_path("..", __FILE__)
+def data_path
+  if ENV["RACK_ENV"] == "test"
+    File.expand_path("../test/data", __FILE__)
+  else
+    File.expand_path("../data", __FILE__)
+  end
+end
 
 def render_markdown(text)
   markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML)
@@ -29,7 +34,8 @@ def load_file_content(path)
 end
 
 get "/" do
-  @files = Dir.glob(root + "/data/*").map do |path|
+  pattern = File.join(data_path, "*")
+  @files = Dir.glob(pattern).map do |path|
     File.basename(path)
   end
   
@@ -37,7 +43,7 @@ get "/" do
 end
 
 get "/:filename" do
-  path = root + "/data/" + params[:filename]
+  path = File.join(data_path, params[:filename])
 
   if File.exist?(path)
     load_file_content(path)
@@ -48,7 +54,7 @@ get "/:filename" do
 end
 
 get "/:filename/edit" do
-  path = root + "/data/" + params[:filename]
+  path = File.join(data_path, params[:filename])
   
   @filename = params[:filename]
   @content = File.read(path)
@@ -57,7 +63,7 @@ get "/:filename/edit" do
 end
 
 post "/:filename" do
-  path = root + "/data/" + params[:filename]
+  path = File.join(data_path, params[:filename])
 
   File.write(path, params[:content])
 
--- test/cms_test.rb
+++ test/cms_test.rb
@@ -2,17 +2,36 @@ ENV["RACK_ENV"] = "test"
 
 require "minitest/autorun"
 require "rack/test"
+require "fileutils"
 
 require_relative "../cms"
 
 class CmsTest < Minitest::Test
   include Rack::Test::Methods
 
+  def setup
+    FileUtils.mkdir_p(data_path)
+  end
+
+  def teardown
+    FileUtils.rm_rf(data_path)
+  end
+
+  def create_document(name, content = "")
+    File.open(File.join(data_path, name), "w") do |file|
+      file.write(content)
+    end
+  end
+
   def app
     Sinatra::Application
   end
 
   def test_index
+    create_document "about.md"
+    create_document "changes.txt"
+    create_document "history.txt"
+
     get "/"
 
     assert_equal 200, last_response.status
@@ -23,11 +42,23 @@ class CmsTest < Minitest::Test
   end
 
   def test_viewing_history_document
+    create_document "history.txt", "1993 - Yukihiro Matsumoto dreams up Ruby."
+
     get "/history.txt"
 
     assert_equal 200, last_response.status
     assert_equal "text/plain", last_response["Content-Type"]
     assert_includes last_response.body, "1993 - Yukihiro Matsumoto dreams up Ruby."
+  end
+
+  def test_viewing_markdown_document
+    create_document "about.md", "# Ruby is..." 
+
+    get "/about.md"
+
+    assert_equal 200, last_response.status
+    assert_equal "text/html;charset=utf-8", last_response["Content-Type"]
+    assert_includes last_response.body, "<h1>Ruby is...</h1>"
   end
 
   def test_not_found
@@ -49,15 +80,9 @@ class CmsTest < Minitest::Test
     refute_includes last_response.body, "The specified file was not found."
   end
 
-  def test_viewing_markdown_document
-    get "/about.md"
-
-    assert_equal 200, last_response.status
-    assert_equal "text/html;charset=utf-8", last_response["Content-Type"]
-    assert_includes last_response.body, "<h1>A File-based Content Management System (CMS)</h1>"
-  end
-
   def test_editing_document
+    create_document "changes.txt"
+    
     get "/changes.txt/edit"
 
     assert_equal 200, last_response.status
@@ -65,17 +90,17 @@ class CmsTest < Minitest::Test
     assert_includes last_response.body, %q(<button type="submit")
   end
 
-  # def test_updating_document
-  #   post "/changes.txt", content: "new content"
+  def test_updating_document
+    post "/changes.txt", content: "new content"
 
-  #   assert_equal 302, last_response.status
+    assert_equal 302, last_response.status
 
-  #   get last_response["location"]
+    get last_response["location"]
 
-  #   assert_includes last_response.body, "changes.txt has been updated"
+    assert_includes last_response.body, "changes.txt has been updated"
 
-  #   get "/changes.txt"
-  #   assert_equal 200, last_response.status
-  #   assert_includes last_response.body, "new content"
-  # end  
+    get "/changes.txt"
+    assert_equal 200, last_response.status
+    assert_includes last_response.body, "new content"
+  end  
 end
